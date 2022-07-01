a1092306b802bdb023e2fd650f16275979805159
AdityaP1502 <i.m.aditya015@gmail.com>
2022-07-01 18:18:45 +0700

    Add Evaluate class to the module, normalized the function param for eval and rules

--- evaluate.py
+++ evaluate.py
@@ -1,11 +1,72 @@
 import numpy as np
 
+class Evaluate():
+    @staticmethod
+    def add(x : float, y : float, extra_param : float = None) -> None:
+        # add value of the input (from)
+        return x + y
+        
+    @staticmethod
+    def sub(x : float, y : float, extra_param : float = None) -> None:
+        # subtract value of the input (from)
+        return x - y
+    
+    @staticmethod
+    def mul(x : float, y : float, extra_param : float = None) -> None:
+        # multiply value of the input (from)
+        return x * y
+        
+    @staticmethod
+    def div(x : float, y : float, extra_param : float = None) -> None:
+        # divide value of the input (from)
+        return x / y
+        
+    @staticmethod
+    def pow(x : float, y : float = None, extra_param : float = None) -> None:
+        return x ** extra_param
+        
+    @staticmethod
+    def exponent(x : float, y : float = None, extra_param : float = None) -> None:
+        return np.e ** (x)
+        
+    @staticmethod
+    def ln(x : float, y : float = None, extra_param : float = None) -> None:
+        return np.log(x)
+        
+    @staticmethod
+    def sin(x : float, y : float = None, extra_param : float = None) -> None:
+        return np.sin(x)
+        
+    @staticmethod
+    def cos(x : float, y : float = None, extra_param : float = None) -> None:
+        return np.cos(x)
+      
+    @staticmethod
+    def tan(x : float, y : float = None, extra_param : float = None) -> None:
+        return np.tan(x)
+        
+    @staticmethod
+    def asin(x : float, y : float = None, extra_param : float = None) -> None:
+        return np.arcsin(x)
+    
+    @staticmethod   
+    def acos(x : float, y : float = None, extra_param : float = None) -> None:
+        return np.arccos(x)
+        
+    @staticmethod    
+    def atan(x : float, y : float = None, extra_param : float = None) -> None:
+        return np.arctan(x)
+        
+    @staticmethod    
+    def abs(x : float, y : float = None, extra_param : float = None) -> None:
+        return np.abs(x)
+    
 class Rules():
     """Derivative Rules. 
     For Reveres Method : Rules Function Evaluate dn_i / dn_j where n_i is the to node and n_j is the from node.
     """
     @staticmethod
-    def add(method : str) -> int:
+    def add(method : str, extra_param : any) -> int:
         if method == "reverse":
             return 1
         
@@ -16,7 +77,9 @@ class Rules():
             NotImplemented
             
     @staticmethod
-    def sub(method : str, isPositive : bool) -> int:
+    def sub(method : str, extra_param : any) -> int:
+        isPositive = extra_param[0]
+        
         if method == "reverse":
             return 1 if isPositive else -1
         
@@ -27,16 +90,8 @@ class Rules():
             NotImplemented
             
     @staticmethod
-    def mul(method : str, other_input_val : float) -> float:
-        """n_i = n_j * n_k
-
-        Args:
-            method (str): "reverse" or "forward"
-            other_input_val (float): if try to find dn_i / dn_j then other_input_val would be n_k.val, etv
-
-        Returns:
-            float : result
-        """
+    def mul(method : str, extra_param : any) -> float:
+        other_input_val = extra_param[0]
         
         if method == "reverse":
             return other_input_val
@@ -48,7 +103,9 @@ class Rules():
             NotImplemented
             
     @staticmethod        
-    def div(method : str, isNumerator : bool, from_node_val : list[int]):
+    def div(method : str, extra_param : any) -> float:
+        isNumerator, from_node_val =  extra_param[:2]
+        
         # numerator = 0, denominator = 1
         numenVal, denomVal = from_node_val
         
@@ -62,7 +119,9 @@ class Rules():
             NotImplemented
             
     @staticmethod        
-    def pow(method : str, power : int, from_node_val):
+    def pow(method : str, extra_param : any) -> float:
+        power, from_node_val = extra_param[:2]
+        
         if method == "reverse":
             return power * from_node_val ** (power - 1)
         
@@ -73,7 +132,9 @@ class Rules():
             NotImplemented
             
     @staticmethod        
-    def exponent(method : str, node_val : float):
+    def exponent(method : str, extra_param : any) -> float:
+        node_val = extra_param[0]
+        
         if method == "reverse":
             return node_val
         
@@ -84,7 +145,8 @@ class Rules():
             NotImplemented
             
     @staticmethod        
-    def ln(method : str, from_node_val : float):
+    def ln(method : str, extra_param : any) -> float:
+        from_node_val = extra_param[0]
         if method == "reverse":
             return 1 / from_node_val
         
@@ -95,7 +157,8 @@ class Rules():
             NotImplemented
             
     @staticmethod    
-    def sin(method : str, from_node_val : float):
+    def sin(method : str, extra_param : any) -> float:
+        from_node_val = extra_param[0]
         if method == "reverse":
             return np.cos(from_node_val)
         
@@ -106,7 +169,8 @@ class Rules():
             NotImplemented
             
     @staticmethod        
-    def cos(method : str, from_node_val : float):
+    def cos(method : str, extra_param : any) -> float:
+        from_node_val = extra_param[0]
         if method == "reverse":
             return -1 * (np.sin(from_node_val))
         
@@ -117,7 +181,8 @@ class Rules():
             NotImplemented
             
     @staticmethod        
-    def tan(method : str, from_node_val : float):
+    def tan(method : str, extra_param : any) -> float:
+        from_node_val = extra_param[0]
         if method == "reverse":
             return (1 / np.cos(from_node_val)) ** 2
         
@@ -126,8 +191,10 @@ class Rules():
             
         else:
             NotImplemented
+            
     @staticmethod        
-    def asin(method : str, from_node_val : float):
+    def asin(method : str, extra_param : any) -> float:
+        from_node_val = extra_param[0]
         if method == "reverse":
             return 1 / np.sqrt(1 - from_node_val ** 2)
         
@@ -138,7 +205,8 @@ class Rules():
             NotImplemented
             
     @staticmethod        
-    def acos(method : str, from_node_val : float):
+    def acos(method : str, extra_param : any) -> float:
+        from_node_val = extra_param[0]
         if method == "reverse":
             return -1 * (1 / np.sqrt(1 - from_node_val ** 2))
         
@@ -149,7 +217,8 @@ class Rules():
             NotImplemented
             
     @staticmethod        
-    def atan(method : str, from_node_val : float):
+    def atan(method : str, extra_param : any) -> float:
+        from_node_val = extra_param[0]
         if method == "reverse":
             return 1 / (1 + from_node_val ** 2)
         
@@ -160,7 +229,8 @@ class Rules():
             NotImplemented
             
     @staticmethod        
-    def abs(method : str, from_node_val : float):
+    def abs(method : str, extra_param : any) -> int:
+        from_node_val = extra_param[0]
         if method == "reverse":
             if from_node_val == 0:
                 # division 0 with 0
