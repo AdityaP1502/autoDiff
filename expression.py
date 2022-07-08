44c76e8cc695ce8288a2b86aefa6b0e918988848
AdityaP1502 <i.m.aditya015@gmail.com>
2022-07-09 05:03:40 +0700

    expression : Automatic add constant node, fix conn shared id for each instance, fix extra_param isn't added in instance variable

--- expression.py
+++ expression.py
@@ -3,7 +3,7 @@
 from evaluate import Rules, Evaluate
 
 class Expression:
-    def __init__(self, operation_id : int, dRules : "function" = None, evalFunc  : "function" = None, connection : list[list["Expression"]] = [[], []], value : float = None, extra_param : int = None) -> None:
+    def __init__(self, operation_id : int, connection : list[list["Expression"]], dRules : "function" = None, evalFunc  : "function" = None, value : float = None, extra_param : float = None) -> None:
         # connection is a 2D array. 0th index = From and 1th index = To
         # Extra param only accessed by a specific function such as power and general logartihm and exponent
         self.op = operation_id
@@ -14,14 +14,29 @@ class Expression:
         self.eval = evalFunc # used to evaluate the node value
 
     @classmethod
-    def constant(cls, value : float) -> "Expression":
+    def __expressionCheck(cls, b : any) -> "Expression":
+        # check if passed a non expression object
+        # if passed float or int, change into constant expression
+        # else throw an error
+        b_type = type(b)
+        if  b_type == type(1) or b_type == type(1.1):
+            b = cls.__constant(b)
+            
+        elif b_type == type(""):
+            # Throw an error
+            NotImplemented
+            
+        return b
+            
+    @classmethod
+    def __constant(cls, value : float) -> "Expression":
         """Create a constant
 
         Returns:
             Expression: Constant Expression
         """
-        
-        return cls(-1, value = value)
+        conn = [[], []]
+        return cls(-1, conn, value = value)
     
     @classmethod
     def variable(cls) -> "Expression":
@@ -29,11 +44,11 @@ class Expression:
         Returns:
             Expression: Variable expression used to define a function 
         """
-        
-        return cls(0)
+        conn = [[], []]
+        return cls(0, conn)
 
     @classmethod
-    def function(cls, operation_id : int, connection : list[list["Expression"]], evalFunc : "function", dRules : "function") -> "Expression":
+    def function(cls, operation_id : int, connection : list[list["Expression"]], evalFunc : "function", dRules : "function", extra_param = None) -> "Expression":
         """Create a function expression node    
         Args:
             operation_id (int): (1 = Addition, 2 = Subtraction, 3 = Multiplication, 4 = Division,
@@ -46,10 +61,10 @@ class Expression:
             Expression: Function Expression
         """
 
-        return cls(operation_id, dRules, evalFunc, connection)
+        return cls(operation_id, connection, dRules, evalFunc, extra_param = extra_param)
 
     @classmethod
-    def __operation(cls, a : "Expression", op_id : int, evalFunc : "function", dRules : "function", b : "Expression" = None, extra_param = None, ) -> "Expression":
+    def __operation(cls, a : "Expression", op_id : int, evalFunc : "function", dRules : "function", b : "Expression" = None, extra_param = None) -> "Expression":
         """Create an operation node and update the input connection to include the new operation node   
         Args:
             b (Expression): other expression 
@@ -59,7 +74,7 @@ class Expression:
         """
 
         # Create a new node with operation add that connect to node a. New Node Will From A. a will go to new Node
-        func_node = cls.function(operation_id = op_id, dRules = dRules, evalFunc = evalFunc, connection=[[a], []]) # put a in from array
+        func_node = cls.function(operation_id = op_id, dRules = dRules, evalFunc = evalFunc, connection=[[a], []], extra_param = extra_param) # put a in from array
         a.conn[1].append(func_node) # Put in in to array
         
         # If exist another input
@@ -71,21 +86,29 @@ class Expression:
     
     # Basic Arithmetic Operation 
     def __add__(self, b : "Expression") -> "Expression":
+        b = self.__expressionCheck(b) # make sure b is an expression 
+        
         evalFnc = Evaluate.add
         dRules = Rules.add
         return self.__operation(self, 1, evalFnc, dRules, b)
 
     def __sub__(self, b : "Expression") -> "Expression":
+        b = self.__expressionCheck(b) # make sure b is an expression 
+        
         evalFnc = Evaluate.sub
         dRules = Rules.sub
         return self.__operation(self, 2, evalFnc, dRules, b)
 
     def __mul__(self, b : "Expression") -> "Expression":
+        b = self.__expressionCheck(b) # make sure b is an expression
+        
         evalFnc = Evaluate.mul
         dRules = Rules.mul
         return self.__operation(self, 3, evalFnc, dRules, b)
 
     def __truediv__(self, b : "Expression") -> "Expression":
+        b = self.__expressionCheck(b) # make sure b is an expression 
+        
         evalFnc = Evaluate.div
         dRules = Rules.div
         return self.__operation(self, 4, evalFnc, dRules, b)
@@ -159,5 +182,5 @@ if __name__ == "__main__":
     y = Expression.variable()
     
     # create an function equation
-    expr = Expression.sin(x + x*(y ** 2)) + Expression.cos(x * y) + Expression.constant(5)
+    expr = Expression.sin(x + x*(y ** 2)) + Expression.cos(x * y) + 5
     print(expr)
